865ea786e216645debee484f40ffdf358c5713af
ortizpy <emanuel.orga@outlook.com>
2025-07-23 11:17:04 -0300

    Cambiando el backends.py de email=None a username=None

--- usuarios/backends.py
+++ usuarios/backends.py
@@ -2,7 +2,8 @@ from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
 class EmailBackend(ModelBackend):
-    def authenticate(self, request, email=None, password=None, **kwargs):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        email = username
         UserModel = get_user_model()
         try:
             user = UserModel.objects.get(email=email)
