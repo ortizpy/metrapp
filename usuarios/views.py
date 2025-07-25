6b36d94a3998c4af7b93d91910c8defed181f396
ortizpy <emanuel.orga@outlook.com>
2025-07-25 08:55:36 -0300

    Cambiando views, ingreso sin autenticacion temporal

--- usuarios/views.py
+++ usuarios/views.py
@@ -12,6 +12,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 from .serializers import UsuarioRegistroSerializer
 
 @csrf_exempt
@@ -29,6 +30,7 @@ def login_api(request):
     
 
 class LoginAPIView(APIView):
+    permission_classes = [AllowAny]  # Permitir acceso sin autenticación
     def post(self, request):
         email = request.data.get('email')
         password = request.data.get('password')
