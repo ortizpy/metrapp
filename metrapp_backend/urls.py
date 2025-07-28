79a39c2baba6e5bf41b39c688d52826bdae51b7a
ortizpy <emanuel.orga@outlook.com>
2025-07-28 10:50:43 -0300

    actualizando JWT

--- metrapp_backend/urls.py
+++ metrapp_backend/urls.py
@@ -9,6 +9,7 @@ from drf_yasg.views import get_schema_view
 from gestion_instrumentos.views import init_csrf
 from drf_yasg import openapi
 from rest_framework_simplejwt.views import TokenObtainPairView
+from gestion_instrumentos.views import CustomTokenObtainPairView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -31,4 +32,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
 ]
--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Usuario
 from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth import authenticate
 
 class UsuarioRegistroSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
@@ -19,3 +21,19 @@ class UsuarioRegistroSerializer(serializers.ModelSerializer):
         validated_data.pop('password2')
         user = Usuario.objects.create_user(**validated_data)
         return user
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    username_field = 'email'
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(email=email, password=password)
+        if user is None:
+            raise serializers.ValidationError("Credenciales inválidas o cuenta inactiva.")
+        if not user.is_active:
+            raise serializers.ValidationError("La cuenta está desactivada.")
+
+        data = super().validate(attrs)
+        return data
